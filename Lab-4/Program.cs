a920e4c29c4bca21eb5a7a6c7991abef046c86a1
Cuyeshi Yunkay <cuyeshi@mail.ru>
2023-11-09 12:15:06 +0300

    Тили-тили, трали-вали

--- Lab-4/Program.cs
+++ Lab-4/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using StringHandler;
+using System;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Lab_4
 {
@@ -10,6 +9,84 @@ namespace Lab_4
     {
         static void Main(string[] args)
         {
+            string inputString;
+            Console.WriteLine("Введите строку:");
+            inputString = Console.ReadLine();
+            Console.WriteLine(DividingTheString(inputString));
+            Console.ReadKey();
+
+            bool isRun = true;
+            while (isRun) 
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine();
+                Console.WriteLine();
+
+                switch (ReadInt())
+                {
+                    case 1:
+                        Console.WriteLine("Введите строку:");
+                        inputString = Console.ReadLine();
+                        Console.WriteLine(DividingTheString(inputString));
+                        break;
+                    
+                    case 2:
+                        break;
+                    
+                    case 3:
+                        break;
+                    
+                    case 0:
+                        break;
+                    
+                    default: 
+                        Console.WriteLine("Введите корректный номер пункта!");
+                        break;
+                }
+            }
+
         }
+
+        static public string DividingTheString(string String)
+        {
+            bool isTrue;
+            string resString = "";
+            resString = resString + String[0];
+            for (int i = 1; i < String.Length; i++)
+            {
+                isTrue = false;
+                for (int j = 0; j < resString.Length; j++)
+                {
+                    if (resString[j] == String[i] || isTrue)
+                    {
+                        isTrue = true;
+                        break;
+                    }
+                }
+                if (!isTrue)
+                {
+                    resString = resString + String[i];
+                }
+            }
+            return resString;
+        }
+
+        /// <summary>
+        /// Метод для проверки вводимых целочисленных данных на корректность.
+        /// </summary>
+        /// <returns></returns>
+        public static int ReadInt()
+        {
+            string numeral = Console.ReadLine();
+            int value;
+            while (!Int32.TryParse(numeral, out value))
+            {
+                Console.WriteLine("Введённые данные не подходят. Введите корректное значение: ");
+                numeral = Console.ReadLine();
+            }
+            return value;
+        }
+
+
     }
 }
---
+++ StringHandler/StringHandler.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace StringHandler
+{
+    public class StringHandler
+    {
+
+
+        public static bool ValidateInput(string input)
+        {
+            string pattern = @"^\+\d+ [0-23]{2}:[0-23]{2} \d+$"; // Формат: +XXXXXXXXXXXXX 00:00 XXX
+            return Regex.IsMatch(input, pattern);
+        }
+
+        public string DividingTheString(string String)
+        {
+            bool isTrue;
+            string resString = "";
+            resString = resString + String[0];
+            for (int i = 1; i < String.Length; i++)
+            {
+                isTrue = false;
+                for (int j = 0; j < resString.Length; j++)
+                {
+                    if (resString[j] == String[i] || isTrue)
+                    {
+                        isTrue = true;
+                        break;
+                    }
+                }
+                if (!isTrue)
+                {
+                    resString = resString + String[i];
+                }
+            }
+            return resString;
+        }
+    }
+}
+
