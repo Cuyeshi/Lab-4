1b9beb49403ac8ea9a10d97da789e0be5cd370bf
Cuyeshi Yunkay <cuyeshi@mail.ru>
2023-11-29 20:17:49 +0300

    Добавлен проект для тестов

---
+++ TestForStrings/TestsForStrings.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestForStrings
+{
+    [TestClass]
+    public class TestsForStrings
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
