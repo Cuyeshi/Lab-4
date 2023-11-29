4dbf5e9d2c446e8b4b0e9d7d5cdd8765b775b004
Cuyeshi Yunkay <cuyeshi@mail.ru>
2023-11-29 20:34:15 +0300

    Добавлено два теста

--- TestForStrings/TestsForStrings.cs
+++ TestForStrings/TestsForStrings.cs
@@ -1,14 +1,44 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using LibraryForStringHandler;
+
 
 namespace TestForStrings
 {
     [TestClass]
     public class TestsForStrings
     {
+        private static bool CompareStrings(string firstString, string secondString) 
+        { 
+            if (firstString == secondString)
+                return true; 
+            else 
+                return false;
+        }
+
+        /// <summary>
+        /// Тест, проверяющий работоспособность case 1 при вводе одного слова в строке.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void OneWordTestCase1()
         {
+            string inputString = "Русские";
+            string convertedString = StringHandler.DividingTheString(inputString);
+            string validString = "Руские";
+
+            Assert.IsTrue(CompareStrings(convertedString, validString));
+        }
+
+        /// <summary>
+        /// Тест, проверяющий работоспособность case 1 при вводе одного слова в строке.
+        /// </summary>
+        [TestMethod]
+        public void FewWordsTestCase1()
+        {
+            string inputString = "Русские не сдаются!";
+            string convertedString = StringHandler.DividingTheString(inputString);
+            string validString = "Руские ндаютя!";
+
+            Assert.IsTrue(CompareStrings(convertedString, validString));
         }
     }
 }
